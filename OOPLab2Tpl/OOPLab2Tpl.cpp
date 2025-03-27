74e2487e886fab743092d989b9fe20d35d46de04
EvpurasM <yevpurash.maksym@chnu.edu.ua>
2025-03-27 18:08:12 +0200

    Lab2/1

--- OOPLab2Tpl/OOPLab2Tpl.cpp
+++ OOPLab2Tpl/OOPLab2Tpl.cpp
@@ -1,38 +1,25 @@
-﻿// OOPLab2Tpl.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
 using namespace std;
-#include "Tasks.h"
-#include "Examples.h"
 
+int compute_expression(int a, int b, int c, int d) {
+    int term1 = (b << 5) + b;  // 33 * b
+    int term2 = (((d << 3) + (d << 2) + (d << 1) + d) + ((a << 3) + (a << 2))) >> 9;  // (d * 15 + 12 * a) / 512
+    int term3 = (c << 6) + c;  // 65 * c
+    int term4 = (d << 3) + (d << 2) + (d << 1);  // d * 14
 
-/// @brief 
-/// @return 
-int main()
-{
-    cout << "OOP. Template for laboratory work #2.\n";
+    return term1 + term2 - term3 + term4;
+}
 
-    char ch = '5';
-    do {
-        system("cls");
-        MenuTask();
-        ch = cin.get();
-        
-        cin.get();
+int main() {
+    int a, b, c, d;
 
-        switch (ch) {
-        case '1': task1();   break;
-        case '2': task1();   break;
-        case '3': task1();   break;
-        case '4': task1();   break;
-		case '5':  Examples(); break;
-		case '6': return 0;
-	    }
-        cout << " Press any key and enter\n";
-        ch = cin.get();
-    } while (ch != '6');
+    cout << "Enter values for a, b, c, d: ";
+    cin >> a >> b >> c >> d;
+
+    int result = compute_expression(a, b, c, d);
+
+    cout << "Computed result: " << result << endl;
 
     return 0;
 }
-
